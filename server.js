3b4f1e96da6f400e0a68d82f20c9ff0318f0390c
WhizWhuz <jesper_andren94@hotmail.com>
2025-04-16 12:32:14 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,41 +1,18 @@
-// Import dependencies
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+
+dotenv.config({ path: "./config.env" });
+
+console.log("📡 Mongo URI:", process.env.MONGO_URI);
+
 const app = require("./app");
 
-// Load environment variables
-dotenv.config();
+const PORT = process.env.PORT || 3000;
 
-// MongoDB connection function using docker 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.DB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("......MongoDB connected successfully!");
-  } catch (error) {
-    console.error("......MongoDB connection error:", error.message);
-    process.exit(1); // Exit the process if the database connection fails
-  }
-};
-
-// Start the server
-const startServer = async () => {
-  try {
-    // Connect to the database
-    await connectDB();
-
-    // Start the Express server
-    const PORT = process.env.PORT || 4000;
-    app.listen(PORT, () => {
-      console.log(`🚀 Server running at http://localhost:${PORT}`);
-    });
-  } catch (error) {
-    console.error("❌ Failed to start the server:", error.message);
-    process.exit(1); // Exit the process if the server fails to start
-  }
-};
-
-// Initialize the server
-startServer();
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log(`Connected to MongoDB🔗`);
+    app.listen(PORT, () => console.log(`👂 Server is listening to ${PORT}`));
+  })
+  .catch((err) => console.error("‼️ MongoDB Connection Error! ", err.message));
