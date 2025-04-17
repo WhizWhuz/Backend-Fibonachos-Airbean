8aa932953aed0f81141e93b91a444ac13ba36a7f
Amokrane Axile <axile@Amokranes-MacBook-Pro.local>
2025-04-17 15:29:34 +0200

    imported globalnidaleware

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const authRoutes = require("./routes/authRoutes");
 const menuRoutes = require("./routes/menuRoutes");
 const aboutRoutes = require("./routes/aboutRoutes");
 const cartRoutes = require("./routes/cartRoutes");
+const globalMiddleware = require("./middleware/globalMiddleware");
 
+
+// FRÅGA OM DETTTA BEHÖVS ?
 // App
 const app = express();
 app.use(express.json());
@@ -23,10 +26,20 @@ app.use((err, req, res, next) => {
   });
 });
 
+//::::::::::::::: FROM  THE GLOBAL MIDDLEWARE FILE
+app.use(globalMiddleware.requestLogger);
+app.use(globalMiddleware.securityHeaders);
+app.use(globalMiddleware.requestTimer);
+
 // Routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/menu", menuRoutes);
 app.use("/api/v1/about", aboutRoutes);
 app.use("/api/v1/cart", cartRoutes);
+
+
+//::::::::::::::::::::::::: Error handling
+app.use(globalMiddleware.notFound);
+app.use(globalMiddleware.errorHandler);
 
 module.exports = app;
