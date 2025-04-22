6808f35aa567eee479325e72a0b6710f3585360a
WhizWhuz <jesper_andren94@hotmail.com>
2025-04-22 14:59:53 +0700

    last changes, a lot of them

--- app.js
+++ app.js
@@ -3,30 +3,15 @@ const authRoutes = require("./routes/authRoutes");
 const menuRoutes = require("./routes/menuRoutes");
 const aboutRoutes = require("./routes/aboutRoutes");
 const cartRoutes = require("./routes/cartRoutes");
-const globalMiddleware = require("./middleware/globalMiddleware");
 const orderRoutes = require("./routes/orderRoutes");
 const profileRoutes = require("./routes/profileRoutes");
+const globalMiddleware = require("./middleware/globalMiddleware");
 
-// App
+// Express
 const app = express();
 app.use(express.json());
 
-/* Middleware to track request time
-app.use((req, res, next) => {
-	req.requestTime = new Date().toISOString();
-	next();
-});
-
-// Error handling middleware (optional but recommended)
-app.use((err, req, res, next) => {
-	console.error(err.stack);
-	res.status(500).json({
-		success: false,
-		message: "Something went wrong!",
-	});
-});*/
-
-//::::::::::::::: FROM  THE GLOBAL MIDDLEWARE FILE
+// Global Middleware
 app.use(globalMiddleware.requestLogger);
 app.use(globalMiddleware.securityHeaders);
 app.use(globalMiddleware.requestTimer);
@@ -39,7 +24,7 @@ app.use("/api/v1/cart", cartRoutes);
 app.use("/api/v1/order", orderRoutes);
 app.use("/api/v1/profile", profileRoutes);
 
-//::::::::::::::::::::::::: Error handling
+// Error handling
 app.use(globalMiddleware.notFound);
 app.use(globalMiddleware.errorHandler);
 
