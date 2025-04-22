5b33bc70a89d7a3f0c48874d2b5533b77cc8fe8b
kelsaur <kellyb.karner@gmail.com>
2025-04-22 09:50:42 +0200

    Added validator for orderId in url

--- middleware/orderMiddleware.js
+++ middleware/orderMiddleware.js
@@ -1,4 +1,6 @@
+const mongoose = require("mongoose");
 const Cart = require("../models/Cart");
+const Order = require("../models/Order");
 
 exports.orderMiddleware = async (req, res, next) => {
 	const userId = req.user.id;
@@ -17,4 +19,24 @@ exports.orderMiddleware = async (req, res, next) => {
 		console.error("Cart check failed: ", error);
 		res.status(500).json({ message: "Server error" });
 	}
+};
+
+exports.validateOrderId = async (req, res, next) => {
+	const { orderId } = req.params;
+
+	if (!mongoose.Types.ObjectId.isValid(orderId)) {
+		return res.status(400).json({
+			success: false,
+			message: `Invalid ID format: ${orderId}`,
+		});
+	}
+
+	const order = await Order.findById(orderId);
+	if (!order) {
+		return res.status(404).json({
+			success: false,
+			message: `Order with ID ${orderId} not found`,
+		});
+	}
+	next();
 };
--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -4,7 +4,10 @@ const router = express.Router();
 const authMiddleware = require("../middleware/authMiddleware");
 const orderController = require("../controllers/orderControllers");
 const adminRoleValidator = require("../middleware/adminRoleValidator");
-const { orderMiddleware } = require("../middleware/orderMiddleware");
+const {
+	orderMiddleware,
+	validateOrderId,
+} = require("../middleware/orderMiddleware");
 
 // Place new order
 router.post("/", authMiddleware, orderMiddleware, orderController.placeOrder);
@@ -18,13 +21,19 @@ router.get(
 );
 
 // Get order status
-router.get("/status/:orderId", authMiddleware, orderController.getOrderStatus);
+router.get(
+	"/status/:orderId",
+	authMiddleware,
+	validateOrderId,
+	orderController.getOrderStatus
+);
 
 // Update order status (admin only)
 router.patch(
 	"/status/:orderId",
 	authMiddleware,
 	adminRoleValidator,
+	validateOrderId,
 	orderController.updateOrderStatus
 );
 
