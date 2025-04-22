9decb2588d1dad0e9d90cea5d111bf8bf8e044b2
WhizWhuz <jesper_andren94@hotmail.com>
2025-04-22 15:33:58 +0700

    error message fix

--- controllers/aboutController.js
+++ controllers/aboutController.js
@@ -5,7 +5,9 @@ exports.getAbout = async (req, res) => {
   try {
     const about = await About.findOne();
     if (!about) {
-      return res.status(404).json({ success: false, message: "About content not found" });
+      return res
+        .status(404)
+        .json({ success: false, message: "About content not found" });
     }
 
     res.status(200).json({ success: true, data: about });
@@ -35,6 +37,12 @@ exports.updateAbout = async (req, res) => {
     res.status(200).json({ success: true, data: about });
   } catch (err) {
     console.error("PUT /about error:", err);
-    res.status(500).json({ success: false, message: "Could not update About content" });
+    res
+      .status(500)
+      .json({
+        success: false,
+        message: "Could not update About content",
+        error_message: err.message,
+      });
   }
 };
